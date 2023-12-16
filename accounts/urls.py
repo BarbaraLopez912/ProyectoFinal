a81d187c077e7b3b6b7773364989dfd0c5201919
Bárbara López Betancourt <blopezbetancourt@gmail.com>
2023-12-16 12:14:12 -0300

    correcciones2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from accounts.views import registro
+from accounts.views import register_request, login_request
 
 urlpatterns = [
-    path('registro/', registro, name="Registro"),
+    path('registro/', register_request, name="Registro"),
+    path('login/', login_request, name="Login"),
+    path('logout/', LogoutView.as_view(template_name="accounts/logout.html"), name="Logout"),
     path('admin/', admin.site.urls),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,45 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from accounts.forms import UserRegisterForm
 
 
-def registro(request):
+def register_request(request):
+    print(f"Method: {request.method}")
+
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
-            return redirect("/app/inicio/")
+            return redirect("Inicio")
 
     form = UserRegisterForm()
     contexto={
         "form": form
     }
     return render(request, "accounts/registro.html", contexto)
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            usuario = data.get('username')
+            contrasenia = data.get('password')
+
+            user = authenticate(username=usuario, password=contrasenia)
+
+            if user:
+                login(request, user)
+
+        return redirect('/app/inicio/')
+
+    form = AuthenticationForm()
+    contexto = {
+        "form": form
+    }
+    return render(request, "accounts/login.html", contexto)
